a466498eefb42e1dcd0f45061ba1c069bb67690d
Guilherme <l0rd.v4dher@gmail.com>
2024-05-13 15:14:07 -0300

    feat: add Main

--- PilhaFila/src/Main.java
+++ PilhaFila/src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Testando fila
+        Fila fila = new Fila(5);
+
+        System.out.println("Adicionando " + fila.adicionar("Abelha"));
+        System.out.println("Adicionando " + fila.adicionar("Bernardo"));
+        System.out.println("Adicionando " + fila.adicionar("Casa"));
+        System.out.println("Adicionando " + fila.adicionar("Dagrão"));
+        System.out.println("Adicionando " + fila.adicionar("Enzo"));
+
+        System.out.println("A fila está vazia? " + fila.isEmpty());
+        System.out.println("Primeiro elemento da fila: " + fila.primeiro());
+        System.out.println("Tamanho da fila: " + fila.tamanho());
+
+        while(!fila.isEmpty())
+            System.out.println("Removendo da fila: " + fila.remover());
+
+        System.out.println("A fila está vazia? " + fila.isEmpty());
+
+        //Testando pilha
+        Pilha pilha = new Pilha(5);
+
+        System.out.println("\n\n\nAdicionando " + pilha.empilhar("Fábio"));
+        System.out.println("Adicionando " + pilha.empilhar("Geraldo"));
+        System.out.println("Adicionando " + pilha.empilhar("Helicoptero"));
+        System.out.println("Adicionando " + pilha.empilhar("Pajé"));
+
+        System.out.println("A pilha está vazia? " + pilha.isEmpty());
+        System.out.println("Topo da pilha: " + pilha.topo());
+        System.out.println("Tamanho da pilha: " + pilha.tamanho());
+
+        while(!pilha.isEmpty())
+            System.out.println("Removendo da pilha: " + pilha.desempilhar());
+
+        System.out.println("A pilha está vazia? " + pilha.isEmpty());
+
     }
 }
